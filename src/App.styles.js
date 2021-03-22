e4672827938a3500375d7acf5acd11c96db06b19
Alexander <hamster2240@gmail.com>
2021-03-22 20:31:05 +0300

    Impleemnt basic styles for the title.

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,7 +1,18 @@
-import { css } from "lit-element";
+import { css } from 'lit-element';
+
 export const AppStyles = css`
-  .nav-container {
-    display: flex;
-    justify-content: space-evenly;
-  }
+	.nav-container {
+		display: flex;
+		justify-content: space-evenly;
+	}
+
+	h1 {
+		font-family: "Swedbank Headline";
+		font-size: 1.625rem;
+		color: #ee7023;
+		max-width: 1496px;
+		margin: 0 auto;
+		padding: 0 1.25rem;
+		margin-top: 1rem;
+	}
 `;
