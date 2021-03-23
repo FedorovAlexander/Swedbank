be8bc9aea9d56531dcbaa93c9aede6d440e1d739
Alexander <hamster2240@gmail.com>
2021-03-23 12:06:20 +0300

    Improve App styles.

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,18 +1,16 @@
 import { css } from 'lit-element';
 
 export const AppStyles = css`
-	.nav-container {
-		display: flex;
-		justify-content: space-evenly;
-	}
-
 	h1 {
 		font-family: "Swedbank Headline";
 		font-size: 1.625rem;
 		color: #ee7023;
 		max-width: 1496px;
-		margin: 0 auto;
+		margin: 1rem auto;
 		padding: 0 1.25rem;
-		margin-top: 1rem;
+	}
+
+	a {
+		color: #257886;
 	}
 `;
