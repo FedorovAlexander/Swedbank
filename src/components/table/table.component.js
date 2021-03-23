065a941e8d1bf7693b142578f5e14723d8111abe
Alexander <hamster2240@gmail.com>
2021-03-23 12:09:52 +0300

    Improve the table data. Improve table styles.

--- src/components/table/table.component.js
+++ src/components/table/table.component.js
@@ -4,24 +4,30 @@ import { TableStyles } from './table.styles';
 
 const Account = [
 	{
-		name: "Siim Tamm",
-		id: "EE752200221057734534",
+		personalInfo: {
+			name: "Siim Tamm",
+			id: "EE752200221057734534",
+		},
 		balance: "3 342 000.00",
 		credit: "20.00",
 		reserved: "725.00",
 		available: "900.56",
 	},
 	{
-		name: "Marju Lepik",
-		id: "EE752200221057734534",
+		personalInfo: {
+			name: "Marju Lepik",
+			id: "EE752200221057734534",
+		},
 		balance: "50.90",
 		credit: "2 000.00",
 		reserved: "",
 		available: "3 000.00",
 	},
 	{
-		name: "Lina Roosipõld",
-		id: "EE752200221057734534",
+		personalInfo: {
+			name: "Lina Roosipõld",
+			id: "EE752200221057734534",
+		},
 		balance: "12 041.00",
 		credit: "20.00",
 		reserved: "",
@@ -44,20 +50,30 @@ class Table extends LitElement {
 	constructor() {
 		super();
 		this.account = [...Account];
-		this.tableHead = [
-			"Account",
-			" ",
-			"Balance",
-			"Credit",
-			"Reserved",
-			"Available",
-		];
+		this.tableHead = ["Account", "Balance", "Credit", "Reserved", "Available"];
 	}
 
 	render() {
 		return html`
 			<section class="table-block">
 				<div class="table-block__wrapper">
+					<div class="table-block__title-wrapper">
+						<h2 class="table-block__title">Your Swedbank overview</h2>
+						<ul class="table-block__download-list">
+							<li class="table-block__download-link">
+								<span
+									class="table-block__link-icon table-block__link-icon--pdf"
+								></span>
+								PDF
+							</li>
+							<li class="table-block__download-link">
+								<span
+									class="table-block__link-icon table-block__link-icon--xsl"
+								></span>
+								XSL
+							</li>
+						</ul>
+					</div>
 					<table class="table">
 						<tbody>
 							<tr class="table__row table__row--1">
@@ -68,17 +84,31 @@ class Table extends LitElement {
 							${this.account.map(
 								(item, index) => html`
 									<tr class="table__row table__row--2">
-										<td class="table__cell">${item.name}</td>
-										<td class="table__cell">${item.id}</td>
+										<td class="table__cell table__cell--name">
+											<a href="#" class="table__link"
+												>${item.personalInfo.name}</a
+											>
+											${item.personalInfo.id}
+										</td>
 										<td class="table__cell">${item.balance}</td>
 										<td class="table__cell">${item.credit}</td>
 										<td class="table__cell">${item.reserved}</td>
-										<td class="table__cell">${item.available}</td>
+										<td class="table__cell table__cell--available">
+											${item.available} EUR
+										</td>
 									</tr>
 								`
 							)}
-							<tr class="table__row table__row--5">
-								<td class="table-cell"></td>
+							<tr class="table__row table__row--total">
+								<td class="table__cell table__cell--name">Total:</td>
+								<td class="table__cell">5456.56</td>
+								<td class="table__cell">456.56</td>
+								<td class="table__cell"></td>
+								<td
+									class="table__cell table__cell--total-available table__cell--available"
+								>
+									456.56 EUR
+								</td>
 							</tr>
 						</tbody>
 					</table>
--- src/components/table/table.styles.js
+++ src/components/table/table.styles.js
@@ -3,15 +3,15 @@ import { css } from 'lit-element';
 export const TableStyles = css`
 	:host {
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
 		width: 100%;
 	}
 	.table-block {
 		display: block;
 		width: 100%;
-		padding: 0 1rem;
+		max-width: 1536px;
+		padding: 0 0.625rem;
+		background-color: #fff;
+		border-radius: 3px;
 	}
 
 	.table-block__wrapper {
@@ -19,10 +19,61 @@ export const TableStyles = css`
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
+		padding-bottom: 2rem;
+	}
+
+	.table-block__title {
+		font-family: "Swedbank Headline";
+		font-size: 0.8125rem;
+		margin-top: 1rem;
+		padding-left: 0.75rem;
+		text-align: left;
+		width: 100%;
+	}
+
+	.table-block__title-wrapper {
+		display: flex;
+		flex-direction: row;
+		justify-content: spabce-between;
+		align-items: center;
+		width: 100%;
+		padding-right: 0.625rem;
+	}
+
+	.table-block__download-list {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		padding-left: 0;
+		list-style: none;
+		font-family: Arial;
+		font-size: 0.75rem;
+	}
+
+	.table-block__download-link {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		padding-left: 0.75rem;
+		cursor: pointer;
+	}
+
+	.table-block__link-icon {
+		display: block;
+		height: 1.25rem;
+		width: 1.25rem;
+		border-radius: 50%;
+		margin-right: 0.25rem;
+		background-color: #32a3ae;
+		background-image: url("/assets/img/pdf.svg");
 	}
 
 	.table {
 		width: 100%;
+		font-family: Arial;
+		border-collapse: collapse;
 	}
 
 	.table tbody {
@@ -33,6 +84,26 @@ export const TableStyles = css`
 		width: 100%;
 	}
 
+	.table__row--total .table__cell {
+		border-bottom: none;
+		font-weight: 700;
+		padding-bottom: 0.75rem;
+		padding-top: 1.25rem;
+		vertical-align: baseline;
+	}
+
+	.table__cell {
+		padding: 0.75rem;
+		border-bottom: 1px solid #f1eeeb;
+		font-size: 0.825rem;
+	}
+
+	.table__row--total .table__cell--total-available {
+		font-family: "Swedbank Sans";
+		font-size: 1.25rem;
+		font-weight: 400;
+	}
+
 	.table__cell:not(:first-child),
 	.table__cell:not(:last-child) {
 		text-align: center;
@@ -40,9 +111,73 @@ export const TableStyles = css`
 
 	.table__cell:first-child {
 		text-align: left;
+		padding-left: 0.5rem;
+	}
+
+	.table__cell:last-child {
+		text-align: right;
+		padding-right: 0.5rem;
+	}
+
+	.table__head {
+		background-color: #e9f7fb;
+		font-size: 0.825rem;
+		font-weight: 400;
+		padding: 0.625rem;
 	}
 
 	.table__head:first-child {
 		text-align: left;
+		padding-left: 0.5rem;
+	}
+
+	.table__head:last-child {
+		text-align: right;
+	}
+
+	.table__link {
+		color: #257886;
+	}
+
+	@media screen and (max-width: 767px) {
+		.table-block__wrapper {
+			padding-bottom: 0.5rem;
+		}
+
+		.table__row {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+		}
+
+		.table__head,
+		.table__cell {
+			display: none;
+		}
+
+		.table__head:first-child,
+		.table__head:last-child,
+		.table__cell--name,
+		.table__cell--available {
+			display: block;
+			flex: 1;
+			word-break: keep-all;
+			white-space: nowrap;
+		}
+
+		.table__cell--name {
+			flex: 3;
+		}
+	}
+
+	@media screen and (max-width: 420px) {
+		.table__head {
+			font-size: 0.75rem;
+		}
+
+		.table__cell {
+			font-size: 0.75rem;
+		}
 	}
 `;
