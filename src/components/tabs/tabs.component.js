9bb8417148fc2d9860c2051afe95b8add7c06a17
Alexander <hamster2240@gmail.com>
2021-03-24 23:05:56 +0300

    Add basic template for the tabs component.

--- src/components/tabs/tabs.component.js
+++ src/components/tabs/tabs.component.js
@@ -9,14 +9,30 @@ class Tabs extends LitElement {
 	static get properties() {
 		return {
 			isMobile: { type: Boolean },
+			tabsList: { type: Array },
 		};
 	}
 	constructor() {
 		super();
 		this.isMobile = window.innerWidth > 767 ? false : true;
+		this.tabsList = ["Payment", "Calculator"];
 	}
 	render() {
-		return html``;
+		return html`
+			<section class="tabs">
+				<div class="tabs__wrapper">
+					<ul class="tabs__items-list">
+						${this.tabsList.map(
+							(item) => html` <li class="tabs__item">${item}</li> `
+						)}
+					</ul>
+					<div class="tabs__content">
+						<div class="tabs__payment-wrapper">payment</div>
+						<div class="tabs__calculator-wrapper">calculator</div>
+					</div>
+				</div>
+			</section>
+		`;
 	}
 
 	_toggleTabs(e) {
--- src/components/tabs/tabs.styles.js
+++ src/components/tabs/tabs.styles.js
@@ -1,3 +1,26 @@
 import { css } from 'lit-element';
 
-export const TabsStyles = css``;
+export const TabsStyles = css`
+	:host {
+		display: flex;
+		width: 100%;
+	}
+
+	.tabs {
+		display: block;
+		width: 100%;
+		max-width: 1536px;
+		padding: 0 0.625rem;
+		background-color: #fff;
+		border-radius: 3px;
+		margin: 0 auto;
+	}
+
+	.tabs__wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding-bottom: 2rem;
+	}
+`;
