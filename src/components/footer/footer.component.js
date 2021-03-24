548cd93748bd4a5958f33262b4cb41703e0eb9fb
Alexander <hamster2240@gmail.com>
2021-03-24 10:22:07 +0300

    Implement footer css styles (desktop).

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -9,6 +9,8 @@ class Footer extends LitElement {
 	static get properties() {
 		return {
 			footerLinks: { type: Array },
+			contacts: { type: Object },
+			socialMedia: { type: Array },
 		};
 	}
 	constructor() {
@@ -43,21 +45,90 @@ class Footer extends LitElement {
 				],
 			},
 		];
+
+		this.contacts = {
+			phoneNumber: "6 310 310",
+			email: "info@swedbank.ee",
+			title: "SWEDBANK AS",
+			address: "Liivalaia 8, 15040 Tallinn",
+			swift: "HABAEE2X",
+			regNumber: 10060701,
+		};
+
+		this.socialMedia = [
+			{ media: "facebook", link: "https://www.facebook.com/SwedbankEestis/" },
+			{
+				media: "instagram",
+				link: "https://www.instagram.com/swedbankeestis/?hl=en",
+			},
+			{
+				media: "youtube",
+				link: "https://www.youtube.com/channel/UCtkFPufr4LOHpSmy399zQIw",
+			},
+			{ media: "twitter", link: "https://twitter.com/Swedbank" },
+			{ media: "skype", link: "skype:swedbank.eestis?call" },
+		];
 	}
 	render() {
 		return html`
 			<footer class="footer">
 				<div class="footer__wrapper">
-					<div class="footer__contacts"></div>
+					<div class="footer__contacts">
+						<h3 class="footer__title footer__title--contact">Contact</h3>
+						<ul class="footer__list">
+							<li class="footer__contacts-item">
+								<a
+									href="tel:+372${this.contacts.phoneNumber}"
+									class="footer__phone-link"
+								>
+									${this.contacts.phoneNumber}
+								</a>
+							</li>
+							<li class="footer__contacts-item">
+								<a
+									href="mailto:${this.contacts.email}"
+									class="footer__email-link"
+								>
+									${this.contacts.email}
+								</a>
+							</li>
+							<li class="footer__contacts-item">${this.contacts.title}</li>
+							<li class="footer__contacts-item">${this.contacts.address}</li>
+							<li class="footer__contacts-item">
+								SWIFT kood/BIC: ${this.contacts.swift}
+							</li>
+							<li class="footer__contacts-item">
+								Reg. number: ${this.contacts.regNumber}
+							</li>
+						</ul>
+						<div class="footer__socials-links">
+							<ul class="footer__socials-list">
+								${this.socialMedia.map(
+									(item) => html`
+										<li class="footer__socials-item">
+											<a
+												href="${item.link}"
+												class="footer__socials-link"
+												target="_blank"
+												rel="noopener noreferrer"
+												title="${item.media}"
+												style="background-image: url(/assets/img/social-media/${item.media}.svg)"
+											></a>
+										</li>
+									`
+								)}
+							</ul>
+						</div>
+					</div>
 					<div class="footer__links-block">
 						${this.footerLinks.map(
 							(item) => html`
 								<div class="footer__links-section">
-									<h3 class="footer__links-title">${item.title}</h3>
-									<ul class="footer__links-list">
+									<h3 class="footer__title">${item.title}</h3>
+									<ul class="footer__list">
 										${item.linksList.map(
 											(item) => html`<li class="footer__link">
-												<nav-link href="${item.href}" textAlign="left">
+												<nav-link href="${item.href}">
 													${item.linkTitle}
 												</nav-link>
 											</li>`
--- src/components/footer/footer.styles.js
+++ src/components/footer/footer.styles.js
@@ -5,13 +5,138 @@ export const FooterStyles = css`
 		margin-top: auto;
 	}
 
+	.footer {
+		position: relative;
+		background-color: #ebe7e2;
+	}
+
+	.footer:before {
+		content: "";
+		position: absolute;
+		height: 0.3125rem;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		background: linear-gradient(to right, var(--brand-orange), #f4ba44);
+	}
+
 	.footer__links-block {
 		display: flex;
 		flex-direction: row;
 		justify-content: flex-start;
 	}
 
-	.footer__link nav-link a {
-		text-align: left;
+	.footer__wrapper {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		box-sizing: border-box;
+		max-width: 1556px;
+		margin: 0 auto;
+		padding: 1.25rem 1rem 2rem;
+	}
+
+	.footer__contacts {
+		flex: 1;
+	}
+
+	.footer__links-block {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		flex: 3;
+	}
+
+	.footer__links-section {
+		flex: 1;
+	}
+
+	.footer__title {
+		font-family: var(--font-titles);
+		font-weight: 500;
+		font-size: 1.125rem;
+		margin-top: 0;
+		margin-bottom: 0.75rem;
+	}
+
+	.footer__title--contact {
+		margin-bottom: 0.475rem;
+	}
+
+	.footer__list {
+		margin: 0;
+		padding-left: 0;
+		list-style: none;
+	}
+
+	.footer__link {
+		position: relative;
+		padding-left: 0.75rem;
+		font-size: 1rem;
+		font-family: var(--font-main);
+		text-decoration: none;
+	}
+
+	.footer__link:before {
+		content: "";
+		position: absolute;
+		top: calc(50% - 7px);
+		left: 0;
+		width: 0;
+		height: 0;
+		border-top: 6px solid transparent;
+		border-bottom: 6px solid transparent;
+		border-left: 6px solid #512b2b;
+	}
+
+	.footer__link > nav-link {
+		padding: 0.375rem 0;
+		width: fit-content;
+	}
+
+	.footer__link nav-link:hover {
+		text-decoration: underline;
+	}
+
+	.footer__phone-link {
+		font-family: var(--font-headline);
+		font-size: 2rem;
+		color: var(--brand-orange);
+		text-decoration: none;
+	}
+
+	.footer__email-link {
+		display: block;
+		font-family: var(--font-main);
+		color: var(--link-color);
+		margin-top: 0.75rem;
+		margin-bottom: 1.25rem;
+	}
+
+	.footer__contacts-item {
+		font-family: var(--font-main);
+		margin-bottom: 0.125rem;
+	}
+
+	.footer__socials-list {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		list-style: none;
+		padding-left: 0;
+	}
+
+	.footer__socials-item {
+		margin-right: 0.5rem;
+	}
+
+	.footer__socials-link {
+		display: block;
+		height: 1.5rem;
+		width: 1.5rem;
+		border-radius: 50%;
+		background-position: center;
+		background-repeat: no-repeat;
 	}
 `;
