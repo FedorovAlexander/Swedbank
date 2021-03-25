afad05341bf6e4472011a7356652286b3feb8495
Alexander <hamster2240@gmail.com>
2021-03-25 15:59:23 +0300

    Add PaymentForm, Calculator and Greeting components to the banking page.

--- src/pages/banking/banking.page.js
+++ src/pages/banking/banking.page.js
@@ -1,3 +1,6 @@
+import '../../components/calculator/calculator.component';
+import '../../components/greeting/greeting.component';
+import '../../components/payment-form/payment-form.component';
 import '../../components/tabs/tabs.component';
 
 import { html, LitElement } from 'lit-element';
@@ -26,11 +29,16 @@ class Banking extends LitElement {
 			<div class="banking-page">
 				<div class="banking-page__wrapper">
 					<app-tabs .tabsList=${this.tabsList} activeTab="${this.activeTab}">
-						<!-- For new tab add new value to the array and create an html-element 
+						<!-- For new tab add a new value to the array and create an html-element 
 						with a corresponding slot name (magic!). -->
-						<div slot="Payment">pay</div>
-						<div slot="Calculator">calc</div>
+						<div slot="Payment">
+							<app-payment-form></app-payment-form>
+						</div>
+						<div slot="Calculator">
+							<app-calculator></app-calculator>
+						</div>
 					</app-tabs>
+					<app-greeting></app-greeting>
 				</div>
 			</div>
 		`;
