e01471a7fcd27e2041548bed695cf187073a7e0d
Alexander <hamster2240@gmail.com>
2021-03-27 16:34:03 +0300

    Basic temlate and style for the greeting component.

--- src/components/greeting/greeting.component.js
+++ src/components/greeting/greeting.component.js
@@ -1,6 +1,9 @@
-import { html, LitElement } from 'lit-element';
+import "../../components/button/button.component";
+import "../../components/read-more-link/read-more-link.component";
 
-import { GreetingStyles } from './greeting.styles';
+import { html, LitElement } from "lit-element";
+
+import { GreetingStyles } from "./greeting.styles";
 
 class Greeting extends LitElement {
 	static get styles() {
@@ -18,7 +21,36 @@ class Greeting extends LitElement {
 	render() {
 		return html`
 			<section class="greeting">
-				<div class="greeting__wrapper">greeting</div>
+				<div class="greeting__wrapper">
+					<div class="greeting__image">
+						<div class="greeting__figure">
+							<span class="greeting__text">Hello World!</span>
+						</div>
+					</div>
+					<div class="greeting__content">
+						<div class="greeting__text">
+							<h3 class="greeting__title">Welcome to Swedbank!</h3>
+							<p class="greeting__description">
+								With 7.2 million privat customers and more than 574 000
+								corporate and organizational customers. This makes us Sweden's
+								largest bank i terms of number of customers and gives us a
+								leading position in our other home markets of Estonia, Latvia
+								and Lithuania. As a major bank, we are a significant part of the
+								financial system and play an important role in the local
+								cummunities we serve. We are dedicated to helping our customers,
+								our shareholders and society as a whole stay financially sound
+								and sustainable.
+							</p>
+						</div>
+						<div class="greeting__links">
+							<app-read-more-link href="#"></app-read-more-link>
+							<app-button
+								buttonText="Go"
+								backgroundColor="#EE7023"
+							></app-button>
+						</div>
+					</div>
+				</div>
 			</section>
 		`;
 	}
--- src/components/greeting/greeting.styles.js
+++ src/components/greeting/greeting.styles.js
@@ -1,4 +1,4 @@
-import { css } from 'lit-element';
+import { css } from "lit-element";
 
 export const GreetingStyles = css`
 	:host {
@@ -11,17 +11,74 @@ export const GreetingStyles = css`
 		width: 100%;
 		max-width: 1536px;
 		padding: 0 0.625rem;
-		background-color: #fff;
+		background-color: #ebf8f2;
 		border-radius: 3px;
-		margin: 3.5rem auto 1rem;
+		margin: 0 auto 2rem;
 	}
 
 	.greeting__wrapper {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
 		justify-content: center;
 		align-items: center;
 		position: relative;
-		padding-bottom: 2rem;
+		padding: 1rem 0.625rem;
+	}
+
+	.greeting__content {
+		margin-right: 0.625rem;
+		margin-left: 0.625rem;
+	}
+
+	.greeting__title {
+		font-family: var(--font-headline);
+		font-weight: 700;
+		margin: 0;
+	}
+
+	.greeting__description {
+		font-size: 0.75rem;
+		font-family: var(--font-main);
+		line-height: 1.4;
+	}
+
+	.greeting__links {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.greeting__image {
+		margin-left: 0.625rem;
+		margin-right: 0.625rem;
+	}
+
+	.greeting__figure {
+		width: 9.375rem;
+		height: 9.375rem;
+		border-radius: 50%;
+		background-color: #fdc129;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		font-family: var(--font-main);
+		font-weight: 700;
+		color: #fff;
+	}
+
+	.greeting__figure:after {
+		content: "";
+		position: absolute;
+		bottom: -1rem;
+		left: calc(50% - 1.25rem);
+		width: 0;
+		height: 0;
+		border-left: 1.25rem solid transparent;
+		border-right: 1.25rem solid transparent;
+
+		border-top: 1.25rem solid #fdc129;
 	}
 `;
