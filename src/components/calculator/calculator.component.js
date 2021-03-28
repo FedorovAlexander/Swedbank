1b913a70e1621cd3124236e7d3df97bef1818eac
Alexander <hamster2240@gmail.com>
2021-03-28 11:13:00 +0300

    Implement calculator component template and styles (desktop screen).

--- src/components/calculator/calculator.component.js
+++ src/components/calculator/calculator.component.js
@@ -14,6 +14,10 @@ class Calculator extends LitElement {
 			isMobile: { type: Boolean },
 			period: { type: Array },
 			interest: { type: Array },
+			loanValue: { type: Number },
+			rangeMin: { type: Number },
+			rangeMax: { type: Number },
+			monthlyPayment: { type: Number },
 		};
 	}
 	constructor() {
@@ -21,6 +25,10 @@ class Calculator extends LitElement {
 		this.isMobile = window.innerWidth > 767 ? false : true;
 		this.period = [...Period];
 		this.interest = [...Interest];
+		this.loanValue = 32000;
+		this.rangeMin = 32000;
+		this.rangeMax = 320000;
+		this.monthlyPayment = 0;
 	}
 	render() {
 		return html`
@@ -33,44 +41,65 @@ class Calculator extends LitElement {
 						id="payment-form"
 					>
 						<div class="calculator__inputs">
-							<div class="calculator__loan-size">
-								<span class="calculator__loan-value"></span>
-								<label class="calculator__label">
-									Loan size
-									<input
-										type="range"
-										id="loan-size"
-										name="loan-size"
-										min="32000"
-										max="320000"
-									/>
-								</label>
-							</div>
-							<div class="calculator__period">
-								<label class="calculator__label">
-									Period
-									<app-select
-										.options=${this.period}
-										name="Period"
-										?firstOptionSelected=${true}
-									></app-select>
-								</label>
-							</div>
-							<div class="calculator__interest">
-								<label class="calculator__label">
-									Interest
-									<app-select
-										.options=${this.interest}
-										name="Interest"
-										?firstOptionSelected=${true}
-									></app-select>
-								</label>
+							<div class="calculator__inputs-wrapper">
+								<div class="calculator__loan-size">
+									<span class="calculator__loan-value">${this.loanValue}€</span>
+									<label class="calculator__label calculator__label--loan-size">
+										Loan size
+										<input
+											type="range"
+											class="calculator__range"
+											id="loan-size"
+											name="loan-size"
+											min="${this.rangeMin}"
+											max="${this.rangeMax}"
+											step="1000"
+											value="${this.loanValue}"
+											@input="${this._rangeChange}"
+										/>
+										<div class="calculator__range-values">
+											<span class="calculator__range-min"
+												>${this.rangeMin} €</span
+											>
+											<span class="calculator__range-max"
+												>${this.rangeMax} €</span
+											>
+										</div>
+									</label>
+								</div>
+								<div class="calculator__period">
+									<label class="calculator__label">
+										Period
+										<app-select
+											.options=${this.period}
+											name="Period"
+											?firstOptionSelected=${true}
+											?inlineWidth="${true}"
+											units=" years"
+										></app-select>
+									</label>
+								</div>
+								<div class="calculator__interest">
+									<label class="calculator__label">
+										Interest
+										<app-select
+											.options=${this.interest}
+											name="Interest"
+											?firstOptionSelected=${true}
+											?inlineWidth="${true}"
+											units="%"
+										></app-select>
+									</label>
+								</div>
 							</div>
 						</div>
 						<div class="calculator__result">
 							<div class="calculator__monthly">
 								<div class="calculator__monthly-label">Monthly payment</div>
-								<div class="calculator__monthly-result"></div>
+								<div class="calculator__monthly-result">
+									${this.monthlyPayment}
+									<span class="calculator__payment-units">EUR</span>
+								</div>
 							</div>
 							<div class="calculator__button-wrapper">
 								<app-button
@@ -88,6 +117,10 @@ class Calculator extends LitElement {
 
 	_runCalculator(e) {
 		console.log("calculate");
+	}
+
+	_rangeChange(e) {
+		this.loanValue = e.target.value;
 	}
 }
 
--- src/components/calculator/calculator.styles.js
+++ src/components/calculator/calculator.styles.js
@@ -17,7 +17,7 @@ export const CalculatorStyles = css`
 	.calculator__wrapper {
 		display: flex;
 		flex-direction: row;
-		justify-content: space-between;
+		justify-content: flex-end;
 		align-items: flex-start;
 		position: relative;
 		padding-bottom: 2rem;
@@ -28,20 +28,133 @@ export const CalculatorStyles = css`
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
+		max-width: 1230px;
 	}
 
 	.calculator__label {
 		display: flex;
 		flex-direction: row;
-		justify-content: flex-start;
+		justify-content: flex-end;
 		align-items: center;
 		width: 100%;
 		margin-bottom: 0.625rem;
 		font-size: 0.75rem;
+		color: #808080;
+	}
+
+	.calculator__label--loan-size {
+		flex-wrap: wrap;
+	}
+
+	.calculator__label app-select {
+		margin-left: 0.625rem;
 	}
 
 	.calculator__inputs {
 		flex: 2;
+		border-right: 1px solid #f1eeeb;
+		padding-top: 1.125rem;
+	}
+
+	.calculator__inputs-wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		max-width: 26.25rem;
+	}
+
+	.calculator__loan-value {
+		margin-left: 6.5625rem;
+		font-family: var(--font-titles);
+		color: var(--brand-orange);
+		font-weight: 500;
+		font-size: 0.875rem;
+		margin-bottom: 0.625rem;
+		display: inline-block;
+	}
+
+	.calculator__range {
+		max-width: 18.75rem;
+		width: 100%;
+		margin: 0;
+		margin-left: 0.625rem;
+	}
+
+	input[type="range"] {
+		-webkit-appearance: none;
+		width: 100%;
+	}
+
+	input[type="range"]::-webkit-slider-thumb {
+		-webkit-appearance: none;
+	}
+
+	input[type="range"]:focus {
+		outline: none;
+	}
+
+	input[type="range"]::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		background: var(--brand-orange);
+		cursor: pointer;
+		height: 20px;
+		width: 20px;
+		border-radius: 50%;
+		top: -6px;
+		position: relative;
+	}
+
+	input[type="range"]::-moz-range-thumb {
+		background: var(--brand-orange);
+		cursor: pointer;
+		height: 20px;
+		width: 20px;
+		border-radius: 50%;
+		top: -6px;
+		position: relative;
+	}
+
+	input[type="range"]::-webkit-slider-runnable-track {
+		width: 100%;
+		height: 9px;
+		cursor: pointer;
+		background: #eaded7;
+		border-radius: 5px;
+		border: 0.2px solid #eaded7;
+	}
+
+	input[type="range"]:focus::-webkit-slider-runnable-track {
+		background: #eaded7;
+	}
+
+	input[type="range"]::-moz-range-track {
+		width: 100%;
+		height: 9px;
+		cursor: pointer;
+		background: #eaded7;
+		border-radius: 5px;
+		border: 0.2px solid #eaded7;
+	}
+
+	.calculator__loan-size {
+		width: 100%;
+	}
+
+	.calculator__range-values {
+		width: 100%;
+		max-width: 18.75rem;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		margin-top: 0.5rem;
+	}
+
+	.calculator__period {
+		width: 100%;
+	}
+
+	.calculator__interest {
+		width: 100%;
 	}
 
 	.calculator__result {
@@ -61,5 +174,38 @@ export const CalculatorStyles = css`
 		display: flex;
 		flex-direction: row;
 		justify-content: flex-end;
+	}
+
+	.calculator__result {
+		padding-top: 1.5rem;
+		padding-left: 0.625rem;
+	}
+
+	.calculator__monthly {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		font-size: 0.875rem;
+		color: #808080;
+		border-bottom: 1px solid #f1eeeb;
+		padding-bottom: 0.625rem;
+		margin-bottom: 1.25rem;
+	}
+
+	.calculator__monthly-label {
+		font-size: 0.875rem;
+		color: #808080;
+	}
+
+	.calculator__monthly-result {
+		font-size: 1.25rem;
+		font-family: var(--font-titles);
+		color: var(--brand-orange);
+	}
+
+	.calculator__payment-units {
+		font-size: 0.75rem;
+		color: #808080;
 	}
 `;
